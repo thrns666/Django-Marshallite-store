a291d253581318ff534aa60039484c2fb35bedcb
thrns666 <super.tawer@gmail.com>
2023-06-15 23:41:50 +0300

    add catalog page, product page(37%....)

--- thrns666/mainsite/views.py
+++ thrns666/mainsite/views.py
@@ -12,21 +12,27 @@ def index(request):
     db_returns = ShopTable.objects.all()
     return render(request, 'mainsite/index.html', {'title': 'Главная страница', 'head_menu': menu, 'categories': db_returns, 'font_awesome_token': font_awesome_token})
 
+
 def about(request):
     return render(request, 'mainsite/about.html', {'title': 'О данном сайте', 'main_menu': menu})
+
 
 def bot_page(request):
     db_returns = DataTable.objects.all()
     return render(request, 'mainsite/bot_page.html', {'title': 'Страница запросов', 'db_obj': db_returns})
 
+
 def catalog(request):
     db_returns = ShopTable.objects.all()
     return render(request, 'mainsite/catalog_page.html', {'title': 'Каталог', 'db_obj': db_returns})
+
 
 def product_catalog(request):
     db_returns = ShopTable.objects.all()
     return render(request, 'mainsite/products_catalog.html', {'title': 'Каталог чего-то', 'db_obj': db_returns})
 
+
 def product_page(request):
     db_returns = ShopTable.objects.all()
+    print(request.META)
     return render(request, 'mainsite/product_page.html', {'title': 'Страница товара', 'db_obj': db_returns})
