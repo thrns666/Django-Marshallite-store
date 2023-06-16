f60ade1021ee69671fa3dcd13f646b5f46d3f6f1
thrns666 <super.tawer@gmail.com>
2023-06-16 19:47:09 +0300

    add catalog page, product page(39%....)

--- thrns666/mainsite/views.py
+++ thrns666/mainsite/views.py
@@ -14,25 +14,25 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'mainsite/about.html', {'title': 'О данном сайте', 'main_menu': menu})
+    return render(request, 'mainsite/about.html', {'title': 'О данном сайте', 'main_menu': menu, 'font_awesome_token': font_awesome_token})
 
 
 def bot_page(request):
     db_returns = DataTable.objects.all()
-    return render(request, 'mainsite/bot_page.html', {'title': 'Страница запросов', 'db_obj': db_returns})
+    return render(request, 'mainsite/bot_page.html', {'title': 'Страница запросов', 'db_obj': db_returns, 'font_awesome_token': font_awesome_token})
 
 
 def catalog(request):
     db_returns = ShopTable.objects.all()
-    return render(request, 'mainsite/catalog_page.html', {'title': 'Каталог', 'db_obj': db_returns})
+    return render(request, 'mainsite/catalog_page.html', {'title': 'Каталог', 'db_obj': db_returns, 'font_awesome_token': font_awesome_token})
 
 
 def product_catalog(request):
     db_returns = ShopTable.objects.all()
-    return render(request, 'mainsite/products_catalog.html', {'title': 'Каталог чего-то', 'db_obj': db_returns})
+    return render(request, 'mainsite/products_catalog.html', {'title': 'Каталог чего-то', 'db_obj': db_returns, 'font_awesome_token': font_awesome_token})
 
 
 def product_page(request):
     db_returns = ShopTable.objects.all()
     print(request.META)
-    return render(request, 'mainsite/product_page.html', {'title': 'Страница товара', 'db_obj': db_returns})
+    return render(request, 'mainsite/product_page.html', {'title': 'Страница товара', 'db_obj': db_returns, 'font_awesome_token': font_awesome_token})
--- thrns666/thrns666/urls.py
+++ thrns666/thrns666/urls.py
@@ -18,7 +18,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-# from mainsite.views import *
+from mainsite.views import *
 from django.urls import path, include
 
 from thrns666 import settings
