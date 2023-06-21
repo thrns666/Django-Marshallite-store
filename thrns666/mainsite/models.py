d2a7916f031b0b704068ff72376296729cc2640e
thrns666 <super.tawer@gmail.com>
2023-06-21 16:40:14 +0300

    today comm(shet....)

--- thrns666/mainsite/models.py
+++ thrns666/mainsite/models.py
@@ -11,19 +11,37 @@ class DataTable(models.Model):
     photo = models.ImageField(upload_to='photos/%y/')
     description = models.TextField(blank=True)
 
+
     def __str__(self):
         return self.title
 
 
-class ShopTable(models.Model):
+class MainCategory(models.Model):
+    name = models.CharField(max_length=50, db_index=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class SubCategories(models.Model):
+    name = models.CharField(max_length=50, db_index=True)
+    main_cat = models.ForeignKey('MainCategory', on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.title
+
+    # def get_url(self):
+    #     return reverse('product', kwargs={'product_id': self.pk})
+
+class Product(models.Model):
     title = models.CharField(max_length=115, blank=True)
     description = models.TextField(blank=True)
     photo = models.ImageField(upload_to='photos/%y/')
     price = models.CharField(max_length=20, default='0')
     availability = models.BooleanField(default=True)
+    cat = models.ForeignKey('SubCategories', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
 
-    def get_url(self):
-        return reverse('product', kwargs={'product_id': self.pk})
+    # def get_url(self):
+    #     return reverse('product', kwargs={'product_id': self.pk})
