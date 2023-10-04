a67286a43a975d8a60b0669f634fb7fcfdbada94
thrns666 <super.tawer@gmail.com>
2023-10-04 16:52:42 +0300

    order_create def in tasks.py ->edit settings.py

--- thrns666/orders/tasks.py
+++ thrns666/orders/tasks.py
@@ -1,3 +1,11 @@
 from celery import current_task
 from django.core.mail import send_mail
 from .models import Order
+
+def order_created(order_id):
+    order = Order.objects.get(id=order_id)
+    subject = f'{order_id}'
+    message = f'Ваш заказ был успешно оформлен, ждем вас еще{order.first_name}, номер вашего заказа {order_id}.'
+
+    mail_send = send_mail(subject, message, 'marshallite@mainsite.ru', [order.email])
+    return mail_send
