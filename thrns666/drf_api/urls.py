5f1cd49da86b4022a968d2fef3cf2b0db75cad10
thrns666 <super.tawer@gmail.com>
2023-10-13 16:51:34 +0300

    drf views.py

--- thrns666/drf_api/urls.py
+++ thrns666/drf_api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from drf_api.views import MarshalliteAPIView
+from drf_api.views import MarshalliteAPIView, UserAPIView
 
 urlpatterns = [
-    path('product-list/', MarshalliteAPIView.as_view(), name='api_list')
+    path('v1/product-list/', MarshalliteAPIView.as_view(), name='api_list'),
+    path('v1/user-api-test', UserAPIView.as_view(), name='test')
 ]
--- thrns666/drf_api/views.py
+++ thrns666/drf_api/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from drf_api.serializers import MarshalliteSerializer
 from mainsite.models import Product
 
@@ -7,3 +10,8 @@ from mainsite.models import Product
 class MarshalliteAPIView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = MarshalliteSerializer
+
+
+class UserAPIView(APIView):
+    def get(self, request):
+        return Response({'name': 'Henka'})
