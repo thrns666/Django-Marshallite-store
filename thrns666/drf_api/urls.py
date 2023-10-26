f73a80210eb30a06a0c32d32fa4b5ee462324361
thrns666 <super.tawer@gmail.com>
2023-10-27 00:47:45 +0300

    drf_api.views.py, see it; need tot reg with token not sstaff user and test delete request. test full views.py

--- thrns666/drf_api/urls.py
+++ thrns666/drf_api/urls.py
@@ -2,16 +2,16 @@ from django.urls import path, include, re_path
 from drf_api.views import *
 from rest_framework import routers
 
-# order_router = routers.SimpleRouter()
-# order_router.register('orders', OrderViewSet)
-#
-# product_router = routers.SimpleRouter()
-# product_router.register('products', ProductViewSet)
+order_router = routers.SimpleRouter()
+order_router.register('orders', OrderViewSet)
+
+product_router = routers.SimpleRouter()
+product_router.register('products', ProductViewSet)
 
 urlpatterns = [
-    # path('v1/', include(product_router.urls)),
-    # path('v1/', include(order_router.urls)),
-    path('v1/orders/', OrderAPIList.as_view()),
-    path('v1/ordersdestroy/<int:id>/', OrderAPIDestroy.as_view()),
+    path('v1/', include(product_router.urls)),
+    path('v1/', include(order_router.urls)),
+    # path('v1/orders/', OrderAPIList.as_view()),
+    # path('v1/ordersdestroy/<int:id>/', OrderAPIDestroy.as_view()),
     path('v1/auth/', include('djoser.urls.authtoken'))
 ]
