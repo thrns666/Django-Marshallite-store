a478a0d8a32287bf120ca433641be47ec766a40f
thrns666 <super.tawer@gmail.com>
2023-12-11 16:40:21 +0300

    search_page.html -> working with wrongs need to fix icontains in views.py in title(need to make no register sensivity) and solve bug with bootstrap containers

--- thrns666/mainsite/views.py
+++ thrns666/mainsite/views.py
@@ -71,7 +71,7 @@ class SearchPage(DataMixin, ListView):
     def get_queryset(self):
         query = self.request.GET.get('q')
         obj_list = Product.objects.filter(Q(title__icontains=query) | Q(description__icontains=query))
-        print(obj_list)
+        print(obj_list.all())
         return obj_list
 
     def get_context_data(self, **kwargs):
@@ -103,7 +103,6 @@ class UserProfile(DataMixin, DetailView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-
         c_def = self.get_user_context(user_orders=Order.objects.filter(user_id=self.request.user.id))
         print(context)
         context = {**context, **c_def}
